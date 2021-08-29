df3bc941a777f5d21beff6159b33a796274e1508
Alexandra-Shyshkovskaya <74681434+Alexandra-Shyshkovskaya@users.noreply.github.com>
2021-08-29 20:36:10 +0300

    add modules and change files

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,8 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
-import contactReducer from './contact/contact-reducer';
+import { contactReducer } from './contact';
 import {
-  persistStore,
+  //persistStore,
   persistReducer,
   FLUSH,
   REHYDRATE,
@@ -22,20 +22,22 @@ const middleware = [
   logger,
 ];
 
-const contactsPersistConfig = {
+/* const contactsPersistConfig = {
   key: 'contacts',
   storage,
   blacklist: ['filter'],
-};
+}; */
 
 const store = configureStore({
   reducer: {
-    contacts: persistReducer(contactsPersistConfig, contactReducer),
+    contacts: contactReducer,
   },
   middleware: middleware,
   devTools: process.env.NODE_ENV === 'development',
 });
 
-const persistor = persistStore(store);
+//const persistor = persistStore(store);
 // eslint-disable-next-line
-export default { store, persistor };
+//export default { store, persistor };
+
+export default store;
